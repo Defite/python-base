b4a13c9a7dc5f13a5d90036865ab54e6af49036f
Defite <me@defite.ru>
2018-01-15 23:51:56 +0300

    Initial commit

---
+++ integer.py
@@ -0,0 +1,6 @@
+x = int(input("Enter an integer: "))
+if x < 0:
+    x = 0
+    print("Negative number changed to zero")
+else:
+    print('Value: ', x)
---
+++ loops.py
@@ -0,0 +1,7 @@
+#languages = ['JavaScript', 'Python', 'C++']
+
+#for language in languages:
+#    print('I know {0}'.format(language))
+
+for i in range(0, 10, 2):
+    print(i)
